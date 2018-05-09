752472d950cbf2438cba1e11b66570ff64661dbe
wangzhihaiSF <1599315665@qq.com>
2018-05-09 17:07:47 +0800

    添加水果布局文件

---
+++ README.md
@@ -0,0 +1,26 @@
+# RecyclerViewTest
+和百分比控件类似，RecyclerView 也属于新增控件，需要添加依赖库来使用。
+
+##步骤
+1.修改activity_main.xml
+  ```html
+  <?xml version="1.0" encoding="utf-8"?>
+  <LinearLayout
+      xmlns:android="http://schemas.android.com/apk/res/android"
+      android:layout_width="match_parent"
+      android:layout_height="match_parent">
+  
+      <android.support.v7.widget.RecyclerView
+          android:id="@+id/recycler_view"
+          android:layout_width="match_parent"
+          android:layout_height="match_parent"/>
+  
+  </LinearLayout>
+  ```
+2.添加图片资源
+
+3.新建 Fruit 类
+
+4 添加 fruit_item.xml
+
+5.
--- app/src/main/java/com/wangzhihai/recyclerviewtest/Fruit.java
+++ app/src/main/java/com/wangzhihai/recyclerviewtest/Fruit.java
@@ -4,4 +4,28 @@ package com.wangzhihai.recyclerviewtest;
  * Created by 王治海 on 2018/5/9.
  */
 public class Fruit {
+    private String fruitName;
+    private int fruitImage;
+
+
+    Fruit(String fruitName, int fruitImage) {
+        this.fruitImage = fruitImage;
+        this.fruitName = fruitName;
+    }
+
+    public int getFruitImage() {
+        return fruitImage;
+    }
+
+//    public void setFruitImage(int fruitImage) {
+//        this.fruitImage = fruitImage;
+//    }
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+//    public void setFruitName(String fruitName) {
+//        this.fruitName = fruitName;
+//    }
 }
