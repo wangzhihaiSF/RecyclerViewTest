87db5a17b30395a2f1d703c488c6d39a562e0cc8
wangzhihaiSF <1599315665@qq.com>
2018-05-09 17:27:03 +0800

    添加水果适配器

--- app/src/main/java/com/wangzhihai/recyclerviewtest/FruitAdapter.java
+++ app/src/main/java/com/wangzhihai/recyclerviewtest/FruitAdapter.java
@@ -1,7 +1,55 @@
 package com.wangzhihai.recyclerviewtest;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
 /**
  * Created by 王治海 on 2018/5/9.
  */
-public class FruitAdapter {
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private List<Fruit> mFruitList;
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        this.mFruitList = fruitList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item, parent,
+                false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitName.setText(fruit.getFruitName());
+        holder.fruitImage.setImageResource(fruit.getFruitImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+       ImageView fruitImage;
+       TextView fruitName;
+
+       ViewHolder(View itemView) {
+           super(itemView);
+           fruitImage = itemView.findViewById(R.id.fruit_image);
+           fruitName = itemView.findViewById(R.id.fruit_name);
+
+       }
+   }
 }
